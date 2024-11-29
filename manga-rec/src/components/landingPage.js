1fdc4d8ed89fc8c56d5cc5c849e2ebff56f156ff
Jonathan <jonathan.s9522@gmail.com>
2024-11-29 02:21:17 -0500

    some css

--- manga-rec/src/components/landingPage.js
+++ manga-rec/src/components/landingPage.js
@@ -1,5 +1,7 @@
 import styles from './landingPage.module.css';
 
+
+
 function landingPage() {
     return (
         <div id  = {styles.page}>
@@ -7,7 +9,7 @@ function landingPage() {
                 <button>Profile</button>
                 <button>Saved</button>
             </div>
-            <div className = {styles.body}>
+            <div className = {styles.landing_body}>
                 <div className = {styles.card_container}>
                     <div className = {styles.card}></div>
                     <button id = {styles.save_btn}></button>
