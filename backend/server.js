178646231c7f6a846831a0befbd3bf810d424c3c
Jonathan <jonathan.s9522@gmail.com>
2024-12-27 05:07:43 -0500

    read in from csv file and get unique genres

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('server working');
 });
 app.listen(port, () => {
-    console.log('listening on port 8080');
+    console.log('listening on port 3000');
 });
